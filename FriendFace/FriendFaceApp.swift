ae6259b893da790bfae5c56aab657992a7a2c458
cesarlopezt <cesarlopez2808@gmail.com>
2023-04-02 15:09:51 -0500

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Cesar Lopez on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
